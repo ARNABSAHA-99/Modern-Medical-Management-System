d89793becce2cf689433e3b4655a4957a5001f22
AbdullahAlAmin19-1 <abdullahalamin1211@gmail.com>
2021-04-21 15:04:03 +0600

    Project

---
+++ Modern Medical Management System/Modern Medical Management System/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Modern_Medical_Management_System
+{
+    class DBConnection
+    {
+        public static SqlConnection Connect()
+        {
+            string connString = @"Server=AL-AMIN\SQLEXPRESS;Database=C#_Project;Integrated Security=true;MultipleActiveResultSets=True;";
+            SqlConnection conn = new SqlConnection(connString);
+            return conn;
+        }
+    }
+}
